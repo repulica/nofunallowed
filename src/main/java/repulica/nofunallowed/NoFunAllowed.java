3955c8da7a3089995c3780e037bcd5c4347d44fb
repulica <repulica@protonmail.com>
2023-09-04 05:37:48 -0700

    add obliterate system

--- src/main/java/repulica/nofunallowed/NoFunAllowed.java
+++ src/main/java/repulica/nofunallowed/NoFunAllowed.java
@@ -13,6 +13,7 @@ public class NoFunAllowed implements ModInitializer {
 	public static final TagKey<Item> ITEM_USAGE_IGNORE = TagKey.of(Registries.ITEM.getKey(), new Identifier(MOD_ID, "usage_ignore"));
 	public static final TagKey<Item> ITEM_USAGE_STRICT = TagKey.of(Registries.ITEM.getKey(), new Identifier(MOD_ID, "usage_strict"));
 	public static final TagKey<Item> ITEM_USAGE_NONE = TagKey.of(Registries.ITEM.getKey(), new Identifier(MOD_ID, "usage_none"));
+	public static final TagKey<Item> ITEM_OBLITERATE = TagKey.of(Registries.ITEM.getKey(), new Identifier(MOD_ID, "obliterate"));
 	//todo: can a strict mode even work for blocks they cant use nbt
 	public static final TagKey<Block> BLOCK_USAGE_IGNORE = TagKey.of(Registries.BLOCK.getKey(), new Identifier(MOD_ID, "usage_ignore"));
 	public static final TagKey<Block> BLOCK_USAGE_STRICT = TagKey.of(Registries.BLOCK.getKey(), new Identifier(MOD_ID, "usage_strict"));
--- src/main/java/repulica/nofunallowed/mixin/MixinItemStack.java
+++ src/main/java/repulica/nofunallowed/mixin/MixinItemStack.java
@@ -1,11 +1,18 @@
 package repulica.nofunallowed.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.particle.DustParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -13,10 +20,12 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import repulica.nofunallowed.AdventureHelper;
 import repulica.nofunallowed.NoFunAllowed;
@@ -25,6 +34,10 @@ import repulica.nofunallowed.NoFunAllowed;
 public abstract class MixinItemStack {
 
 	@Shadow public abstract boolean isIn(TagKey<Item> tag);
+
+	@Shadow public abstract void setCount(int count);
+
+	@Shadow public abstract Text getName();
 
 	private boolean canUse(LivingEntity user, World world, Vec3d pos, @Nullable String prefix) {
 		if (isIn(NoFunAllowed.ITEM_USAGE_IGNORE)) return true;
@@ -53,7 +66,7 @@ public abstract class MixinItemStack {
 
 	@Inject(method = "use", at = @At("HEAD"), cancellable = true)
 	private void injectUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> info) {
-		//todo: grab from user/hand instead?
+		//todo: grab from user/hand instead maybe
 		if (!canUse(user, world, user.getPos(), "InAir")) {
 			info.setReturnValue(TypedActionResult.pass(((ItemStack) (Object) this)));
 		}
@@ -72,6 +85,21 @@ public abstract class MixinItemStack {
 	private void injectUseOnEntity(PlayerEntity user, LivingEntity entity, Hand hand, CallbackInfoReturnable<ActionResult> info) {
 		if (!canUse(user, user.getWorld(), user.getPos(), "OnEntity")) {
 			info.setReturnValue(ActionResult.PASS);
+		}
+	}
+
+	@Inject(method = "inventoryTick", at = @At("HEAD"), cancellable = true)
+	private void injectInventoryTick(World world, Entity entity, int slot, boolean selected, CallbackInfo info) {
+		if (isIn(NoFunAllowed.ITEM_OBLITERATE)) {
+			if (entity instanceof PlayerEntity living) {
+				if (!canUse(living, world, entity.getPos(), null) && world instanceof ServerWorld sworld) {
+					living.sendMessage(Text.translatable("message.nofunallowed.obliterate", getName()), true);
+					sworld.playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ITEM_BUNDLE_DROP_CONTENTS, SoundCategory.PLAYERS, 1f, 1f);
+					sworld.spawnParticles(new DustParticleEffect(new Vector3f(1.0f, 1.0f, 1.0f), 1.0f), entity.getX(), entity.getY()+1, entity.getZ(), 20, 0.5, 0.1, 0.5, 1);
+					this.setCount(0);
+					info.cancel();
+				}
+			}
 		}
 	}
 }
